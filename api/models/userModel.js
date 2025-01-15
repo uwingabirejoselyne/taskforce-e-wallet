63208a33c2f5b5e374955aa10dc46f6cc7324b94
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-01-15 09:37:48 +0200

    created json web token expiration time

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -26,4 +26,21 @@ var userSchema = new mongoose.Schema({
         required: true,
       }
 });
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")){
+      next()
+    }
+    const salt = await bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  });
+  userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+  userSchema.methods.createPasswordResetToken = async function (){
+    const resetToken = crypto.randomBytes(32).toString("hex")
+    this.passwordResetToken = crypto.createHash('sha256').update('resettoken').digest("hex")
+    this.passwordChangeAt = Date.now() +30*60*1000;
+    return resetToken
+  }
 module.exports = mongoose.model('User', userSchema);
