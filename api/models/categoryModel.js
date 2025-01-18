41371ccd1369da8dcd5b4c29a5252073e51ca46c
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-01-18 16:53:03 +0200

    created add category method

--- api/models/categoryModel.js
+++ api/models/categoryModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  parentCategory: { type: mongoose.Schema.Types.ObjectId, ref: "Category", default: null },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  parentCategory: { type: String, ref: "Category", default: null },
+  userId: { type: String, ref: "User", required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Category", categorySchema);
